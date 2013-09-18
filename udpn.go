f9a1ec194ceb4100660f06ef39bb3611391d3a33
Ben Curren <ben@thecurrens.com>
2013-09-18 14:45:46 -0700

    finished testing Search

--- udpn.go
+++ udpn.go
@@ -1,17 +1,22 @@
 package udpn
 
 import (
-	"net"
-	"net/url"
-	"time"
 	"bufio"
-	"strings"
-	"net/http"
 	"bytes"
+	"net"
+	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
+	"time"
 )
 
-type Response struct {
+const (
+	Port        = 1900
+	BroadcastIP = "239.255.255.250"
+)
+
+type SearchResponse struct {
 	Control  string
 	Server   string
 	ST       string
@@ -22,109 +27,51 @@ type Response struct {
 	Addr     *net.UDPAddr
 }
 
-func Search(st string, mx time.Duration) (res []Response, err error) {
-	conn, err := listenForResponse()
+type searchReader interface {
+	ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error)
+	SetReadDeadline(t time.Time) error
+}
+
+func Search(st string, mx time.Duration) (responses []SearchResponse, err error) {
+	conn, err := listenForSearchResponses()
 	if conn != nil {
 		defer conn.Close()
 	}
 	if err != nil {
 		return
 	}
-	
-	broadcastAddr, bytes := makeSearchRequest(st, mx)
-	_, err = conn.WriteTo(bytes, broadcastAddr)
+
+	searchBytes, broadcastAddr := buildSearchRequest(st, mx)
+	// Write search bytes on the wire so all devices can respond
+	_, err = conn.WriteTo(searchBytes, broadcastAddr)
 	if err != nil {
 		return
 	}
-	
-	res, err = readResponses(conn, mx)
+
+	responses, err = readSearchResponses(conn, mx)
 	return
 }
 
-func ParseResponse(httpResponse string, responseAddr *net.UDPAddr) (res Response, err error) {
-	request, err := http.NewRequest("M-SEARCH", "239.255.255.250:1900/*", strings.NewReader(""))
-	if err != nil {
-		return
-	}
-	
-	reader := bufio.NewReader(strings.NewReader(httpResponse))
-	response, err := http.ReadResponse(reader, request)
-	if err != nil {
-		return
-	}
-	headers := response.Header
-	
-	res = Response{}
-	
-	res.Control = headers.Get("cache-control")
-	res.Server = headers.Get("server")
-	res.ST = headers.Get("st")
-	res.Ext = headers.Get("ext")
-	res.USN = headers.Get("usn")
-	res.Addr = responseAddr
-	
-	if headers.Get("location") != "" {
-		res.Location, err = response.Location()
-		if err != nil {
-			return
-		}
-	}
-	
-	date := headers.Get("date")
-	if date != "" {
-		res.Date, err = http.ParseTime(date)
-		if err != nil {
-			return
-		}
-	}
-	
-	return
-}
-
-func readResponses(conn *net.UDPConn, duration time.Duration) (responses []Response, err error) {
-	responses = make([]Response, 0, 10)
-	conn.SetReadDeadline(time.Now().Add(duration))
-	
-	buf := make([]byte, 1024)
-	for {
-		rlen, addr, err := conn.ReadFromUDP(buf)
-		if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
-			break // timeout reached, return what we've found
-		}
-		if err != nil {
-			return nil, err
-		}
-		
-		response, err := ParseResponse(string(buf[:rlen]), addr)
-		if err != nil {
-			return nil, err
-		}
-		responses = append(responses, response)
-	}
-	
-	return
-}
-
-func listenForResponse() (conn *net.UDPConn, err error) {
-	serverAddr, _ := net.ResolveUDPAddr("udp", "0.0.0.0:1900")
+func listenForSearchResponses() (conn *net.UDPConn, err error) {
+	serverAddr, _ := net.ResolveUDPAddr("udp", "0.0.0.0:"+strconv.Itoa(Port))
 	conn, err = net.ListenUDP("udp", serverAddr)
 	return
 }
 
-func makeSearchRequest(st string, mx time.Duration) (broadcastAddr *net.UDPAddr, searchBytes []byte) {
-	// Needed to specify * as PATh in HTTP
+func buildSearchRequest(st string, mx time.Duration) (searchBytes []byte, broadcastAddr *net.UDPAddr) {
+	// Placeholder to replace with * later on
 	replaceMePlaceHolder := "/replacemewithstar"
-	
-	broadcastAddr, _ = net.ResolveUDPAddr("udp", "239.255.255.250:1900")
-	request, _ := http.NewRequest("M-SEARCH", 
-		"http://" + broadcastAddr.String() + replaceMePlaceHolder, strings.NewReader(""))
-	
+
+	broadcastAddr, _ = net.ResolveUDPAddr("udp", BroadcastIP+":"+strconv.Itoa(Port))
+	request, _ := http.NewRequest("M-SEARCH",
+		"http://"+broadcastAddr.String()+replaceMePlaceHolder, strings.NewReader(""))
+
 	headers := request.Header
 	headers.Set("User-Agent", "")
 	headers.Set("st", st)
 	headers.Set("man", `"ssdp:discover"`)
-	headers.Set("mx", strconv.FormatInt(int64(mx / time.Second), 10))
-	
+	headers.Set("mx", strconv.Itoa(int(mx/time.Second)))
+
 	searchBytes = make([]byte, 0, 1024)
 	buffer := bytes.NewBuffer(searchBytes)
 	err := request.Write(buffer)
@@ -132,9 +79,72 @@ func makeSearchRequest(st string, mx time.Duration) (broadcastAddr *net.UDPAddr,
 		panic("Fatal error writing to buffer. This should never happen (in theory).")
 	}
 	searchBytes = buffer.Bytes()
-	
-	// Path should be * unescape. This is a hack to accomplish that.
+
+	// Replace placeholder with *. Needed because request always escapes * when it shouldn't
 	searchBytes = bytes.Replace(searchBytes, []byte(replaceMePlaceHolder), []byte("*"), 1)
-	
-	return 
+
+	return
 }
+
+func readSearchResponses(reader searchReader, duration time.Duration) (responses []SearchResponse, err error) {
+	responses = make([]SearchResponse, 0, 10)
+	// Only listen for responses for duration amount of time.
+	reader.SetReadDeadline(time.Now().Add(duration))
+
+	buf := make([]byte, 1024)
+	for {
+		rlen, addr, err := reader.ReadFromUDP(buf)
+		if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+			break // duration reached, return what we've found
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		response, err := parseSearchResponse(string(buf[:rlen]), addr)
+		if err != nil {
+			return nil, err
+		}
+		responses = append(responses, response)
+	}
+
+	return
+}
+
+func parseSearchResponse(httpResponse string, responseAddr *net.UDPAddr) (res SearchResponse, err error) {
+	reader := bufio.NewReader(strings.NewReader(httpResponse))
+	request := &http.Request{} // Needed for ReadResponse but doesn't have to be real
+	response, err := http.ReadResponse(reader, request)
+	if err != nil {
+		return
+	}
+	headers := response.Header
+
+	res = SearchResponse{}
+
+	res.Control = headers.Get("cache-control")
+	res.Server = headers.Get("server")
+	res.ST = headers.Get("st")
+	res.Ext = headers.Get("ext")
+	res.USN = headers.Get("usn")
+	res.Addr = responseAddr
+
+	if headers.Get("location") != "" {
+		res.Location, err = response.Location()
+		if err != nil {
+			return
+		}
+	}
+
+	date := headers.Get("date")
+	if date != "" {
+		res.Date, err = http.ParseTime(date)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+
--- udpn_test.go
+++ udpn_test.go
@@ -1,14 +1,57 @@
 package udpn
 
 import (
-	"testing"
 	"net"
 	"net/url"
+	"testing"
 	"time"
+	"bytes"
 )
 
-func Test_listenForResponse(t *testing.T) {
-	conn, err := listenForResponse()
+type timeoutError struct {	
+}
+
+func (e *timeoutError) Error() string { 
+	return "i/o timeout" 
+}
+
+func (e *timeoutError) Timeout() bool { 
+	return true 
+}
+
+func (e *timeoutError) Temporary() bool { 
+	return true 
+}
+
+type stubSearchReader struct {
+	readIteration int
+	readBuffers []string
+	readAddr *net.UDPAddr
+	storedReadDeadline time.Time
+}
+
+func (s *stubSearchReader) ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error) {
+	// Return timeout error if no more buffers
+	if s.readIteration >= len(s.readBuffers) {
+		return 0, nil, &timeoutError{}
+	}
+	
+	// Create and return the current buffer for the iteration
+	buffer := bytes.NewBufferString(s.readBuffers[s.readIteration])
+	n, err = buffer.Read(b)
+	addr = s.readAddr
+	
+	s.readIteration += 1
+	return
+}
+
+func (s *stubSearchReader) SetReadDeadline(t time.Time) error {
+	s.storedReadDeadline = t
+	return nil
+}
+
+func Test_listenForSearchResponses(t *testing.T) {
+	conn, err := listenForSearchResponses()
 	if err != nil {
 		t.Fatal("Error listening for response.", err)
 	}
@@ -18,7 +61,47 @@ func Test_listenForResponse(t *testing.T) {
 	defer conn.Close()
 }
 
-func Test_ParseResponse(t *testing.T) {
+func Test_buildSearchRequest(t *testing.T) {
+	expectedSearchRequest := "M-SEARCH * HTTP/1.1\r\n" +
+		"Host: 239.255.255.250:1900\r\n" +
+		"Man: \"ssdp:discover\"\r\n" +
+		"Mx: 5\r\n" +
+		"St: upnp:rootdevice\r\n" +
+		"\r\n"
+	expectedBroadcastAddr, _ := net.ResolveUDPAddr("udp", "239.255.255.250:1900")
+
+	searchBytes, broadcastAddr := buildSearchRequest("upnp:rootdevice",
+		time.Duration(5)*time.Second)
+
+	actualSearchRequest := string(searchBytes)
+	if expectedSearchRequest != actualSearchRequest {
+		t.Errorf("Expected search request to be:\n\n%s\n but it was:\n\n%s\n",
+			expectedSearchRequest, actualSearchRequest)
+	}
+
+	assertEqual(t, expectedBroadcastAddr.String(), broadcastAddr.String(), "broadcastAddr")
+}
+
+func Test_readSearchResponses(t *testing.T) {
+	stub := &stubSearchReader{}
+	stub.readAddr, _ = net.ResolveUDPAddr("udp", "192.168.0.12:1000")
+	
+	stub.readBuffers = make([]string, 0, 2)
+	stub.readBuffers = append(stub.readBuffers, "HTTP/1.1 200 OK\r\n\r\n")
+	stub.readBuffers = append(stub.readBuffers, "HTTP/1.1 200 OK\r\n\r\n")
+	
+	responses, err := readSearchResponses(stub, time.Duration(1)*time.Second)
+	if err != nil {
+		t.Fatal("Error while retrieving reading search responses.", err)
+	}
+	
+	if len(stub.readBuffers) != len(responses) {
+		t.Fatalf("Expected %d responses but received %d.", len(stub.readBuffers), 
+			len(responses))
+	}
+}
+
+func Test_parseSearchResponse(t *testing.T) {
 	responseBody := "HTTP/1.1 200 OK\r\n" +
 		"CACHE-CONTROL: max-age=100\r\n" +
 		"EXT:\r\n" +
@@ -28,45 +111,43 @@ func Test_ParseResponse(t *testing.T) {
 		"USN: uuid:2f402f80-da50-11e1-9b23-0017880a4c69::upnp:rootdevice\r\n" +
 		"Date: Sun, 18 Aug 2013 08:49:37 GMT\r\n" +
 		"\r\n"
-		
+
 	responseAddr, _ := net.ResolveUDPAddr("udp", "10.1.2.3:1900")
-	response, err := ParseResponse(responseBody, responseAddr)
+	response, err := parseSearchResponse(responseBody, responseAddr)
 	if err != nil {
 		t.Fatal("Error while parsing the response.", err)
 	}
-	
+
 	assertEqual(t, "max-age=100", response.Control, "response.Control")
 	assertEqual(t, "FreeRTOS/6.0.5, UPnP/1.0, IpBridge/0.1", response.Server, "response.Server")
 	assertEqual(t, "upnp:rootdevice", response.ST, "response.ST")
 	assertEqual(t, "", response.Ext, "response.Ext")
 	assertEqual(t, "uuid:2f402f80-da50-11e1-9b23-0017880a4c69::upnp:rootdevice", response.USN, "response.USN")
 	assertEqual(t, responseAddr, response.Addr, "response.Addr")
-	
+
 	url, _ := url.Parse("http://10.1.2.3:80/description.xml")
 	if url.String() != response.Location.String() {
 		t.Errorf("%q is not equal to %q. %q", url.String(), response.Location.String(), "response.Location")
 	}
-	
+
 	gmt, _ := time.LoadLocation("UTC")
 	date := time.Date(2013, time.August, 18, 8, 49, 37, 0, gmt)
 	assertEqual(t, date, response.Date, "response.Date")
 }
 
-func Test_ParseResponse_NoDateOrLocation(t *testing.T) {
-	responseBody := "HTTP/1.1 200 OK\r\n" +
-		"\r\n"
-		
+func Test_parseSearchResponse_NoDateOrLocation(t *testing.T) {
+	responseBody := "HTTP/1.1 200 OK\r\n\r\n"
 	responseAddr, _ := net.ResolveUDPAddr("udp", "10.1.2.3:1900")
-	response, err := ParseResponse(responseBody, responseAddr)
+	response, err := parseSearchResponse(responseBody, responseAddr)
 	if err != nil {
 		t.Fatal("Error while parsing the response.", err)
 	}
-	
+
 	emptyTime := time.Time{}
 	if response.Date != emptyTime {
 		t.Error("Date should be nil")
 	}
-	
+
 	if response.Location != nil {
 		t.Error("Location should be nil")
 	}
